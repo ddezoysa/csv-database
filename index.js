2bfd647c9bfb9680defb75924d97236c8aa84838
Dinuth De Zoysa <ddezoysa>
2020-07-07 09:35:37 +0000

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
+let csvdb = require('csv-database');
 
-let Swagger = require('swagger-client');
-let apix = require("./apix.json");
+exports.main = async function () {
+    const db = await csvdb("users.csv", ["name", "gender", "address", "position"], ",");
+    
+    // All Users
+    const users = await db.get();
+    console.log('All Users: ');
+    console.log(JSON.stringify(users, undefined, 2));
 
-exports.main = function () {
+    // Get 
+    const andiUser = await db.get({name: "Nita"});
+    console.log('Get Andi: ');
+    console.log(JSON.stringify(andiUser, undefined, 2));
 
-	Swagger.http({
-		url: `https://services.${apix.domain}/api-sandbox/application/token`,
-		method: 'post',
-		query: {},
-		headers: { "Accept": "*/*", "Content-Type": "application/json" },
-		body: JSON.stringify({
-			"userName": apix.credentials.username,
-			"password": apix.credentials.password
-		})
-	}).then((response) => {
-		var access_token = "bearer " + response.body.access_token;
-		console.log(access_token);
-		// Insert new API calls here to call with APIX Access Token
-		
-	}).catch((err) => {
-		console.error(err);
-	});
+    // Add 
+    var dinuthUser = await db.add({name: "Dinuth", gender: "m", address: "London", position: "Architect"});
+    console.log('Add Dinuth: ');
+    console.log(JSON.stringify(dinuthUser, undefined, 2));
 
+    // Edit 
+    // dinuthUser = await db.edit({name: "Dinuth"}, {address: "London UK"});
+    // console.log('Edit Dinuth: ');
+    // console.log(JSON.stringify(dinuthUser, undefined, 2));
+
+    // Delete 
+    // dinuthUser = await db.delete({name: "Dinuth"});
+    // console.log('Delete Dinuth: ');
+    // console.log(JSON.stringify(dinuthUser, undefined, 2));
 }
-
 this.main();
